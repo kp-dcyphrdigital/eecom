e664038b1ffb91d2de18d34e3a26078fdf548a4a
Kazim P <kazim.parkar@syginteractive.com.au>
2018-06-06 09:50:25 +1000

    Refactor Exception Handling in Tests

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -14,30 +14,13 @@ class ProductTest extends TestCase
     /** @test */
     public function products_should_be_filterable_by_category()
     {
-        $category1 = factory('App\Category')->create();
-        $category2 = factory('App\Category')->create();
-
-        $product1 = factory('App\Product')->create([
-        	'name' => 'randomtestproduct1',
-        ]);
-        $category1->products()->attach($product1);
-
-        $product2 = factory('App\Product')->create([
-        	'name' => 'randomtestproduct2',
-        ]);
-        $category2->products()->attach($product2);
-
-        $product3 = factory('App\Product')->create([
-        	'name' => 'randomtestproduct3',
-        ]);
-
-        $categories = $category1->id . ',' . $category2->id;
+        $exitCode = \Artisan::call('db:seed');
+        $categories = '2,4';
         $products = (new Product)->byCategory($categories)
                         ->get()
                         ->toJson();
-
-        $this->assertContains('randomtestproduct1', $products);
-        $this->assertContains('randomtestproduct2', $products);
-        $this->assertNotContains('randomtestproduct3', $products);
+        $this->assertContains('Baur Supreme 1S Ice Hockey Skate', $products);
+        $this->assertContains('Bauer Nexus 1N Griptac Hockey Stick 17', $products);
+        $this->assertNotContains('Bauer S17 Supreme S170 Glove New', $products);
     }
 }
