1267e401c8caf8c2dbecf844fcbb6c86b29c9f45
Kazim P <kazim.parkar@syginteractive.com.au>
2018-06-08 15:47:26 +1000

    Fix nav links, done for the week!

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -17,21 +17,21 @@ class ProductsTableSeeder extends Seeder
             'image' => 'products/skate1.jpg',
             'rating' => 4,
             'featured' => 1,
-        ])->categories()->attach([1,2]);
+        ])->categories()->attach([2]);
 
         factory('App\Product')->create([
             'name' => 'Bauer Nexus 1N Griptac Hockey Stick 17',
             'image' => 'products/stick1.jpg',
             'rating' => 3,
             'featured' => 1,
-        ])->categories()->attach([3,4]);
+        ])->categories()->attach([7]);
 
         factory('App\Product')->create([
             'name' => 'Bauer S17 Supreme S170 Glove New',
             'image' => 'products/glove1.jpg',
             'rating' => 5,
             'featured' => 1,
-        ])->categories()->attach([5,6]);
+        ])->categories()->attach([9]);
 
 		for ($i=0; $i < 10; $i++) { 
             factory('App\Product')->create([
@@ -41,7 +41,7 @@ class ProductsTableSeeder extends Seeder
                 'price' => rand(1, 10) < 3 ? $rrp - 1000 : $rrp,
                 'rating' => rand(0, 5),
                 'featured' => $i < 6 ? 1 : 0,    
-            ])->categories()->attach([1,2]);
+            ])->categories()->attach([2]);
 
             factory('App\Product')->create([
                 'image' => 'products/stick' . rand(1,4) . '.jpg',
@@ -50,7 +50,7 @@ class ProductsTableSeeder extends Seeder
                 'price' => rand(1, 10) < 3 ? $rrp - 1000 : $rrp,
                 'rating' => rand(0, 5),  
                 'featured' => $i < 6 ? 1 : 0,    
-            ])->categories()->attach([3,4]);
+            ])->categories()->attach([7]);
 
             factory('App\Product')->create([
                 'image' => 'products/glove' . rand(1,2) . '.jpg',
@@ -59,7 +59,7 @@ class ProductsTableSeeder extends Seeder
                 'price' => rand(1, 10) < 3 ? $rrp - 1000 : $rrp,
                 'rating' => rand(0, 5),
                 'featured' => $i < 6 ? 1 : 0,
-            ])->categories()->attach([5,6]);
+            ])->categories()->attach([9]);
          }
     }
 }
